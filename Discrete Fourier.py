d79fa934686fc5276bdaef8c1ce48d18c2c8b345
Hwh40 <hwh40@uclive.ac.nz>
2023-05-24 21:58:24 +1200

    Fast fourier transform and plot results

---
+++ Discrete Fourier.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+dt = 0.1
+mu = 0 
+sigma = 0.1
+
+xs = np.arange(0, 10, dt)
+
+def spectrum(x, fs, oversample):
+
+    N = len(x)
+    w = np.hamming(N)
+    g = x * w
+    
+    y = np.zeros(N * oversample)
+    y[0:N] = g
+    Y = np.fft.rfft(y)
+    f = np.fft.rfftfreq(N * oversample, 1 / fs)
+
+    print(f'{N} Samples')
+    print(f'Nyquist frequency = {fs / 2} Hz')
+    print(f'Max frequency = {fs} Hz')
+    print(f'Frequency resolution = {fs / (N * oversample)} Hz')
+    return Y,f 
+    
+def signal(xs):
+    ys = np.sin(2*np.pi*xs) + np.random.normal(mu, sigma, len(xs))
+    return ys
+
+def plot(fourier, freq):
+    f, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey = True)
+    f.suptitle('Fourier Analysis')
+    axes = [ax1, ax2, ax3]
+    for ax in axes:
+        ax.grid(True)
+        ax.set_xlim([0,0.5/dt])
+        ax.set_xlabel('Frequency [Hz]')
+    ax1.set_ylabel('Magnitude')
+    ax2.set_ylabel('Real')
+    ax3.set_ylabel('Imaginary')
+    ax1.plot(freq, abs(fourier),'r')
+    ax2.plot(freq, fourier.real,'g')
+    ax3.plot(freq, fourier.imag,'b')    
+
+def main():
+    ys = signal(xs)
+    fourier, freq = (spectrum(ys, 1/dt, 10))
+    plot(fourier,freq)
+    
+
+if __name__ == '__main__':
+    main()
+    plt.show()
